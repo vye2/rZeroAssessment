894809c7f4d2e50e7b5f0afedf168c3051a7e44a
vye2 <vye2@ucsc.edu>
2021-01-14 13:25:38 -0800

    create spring project with dependencies

---
+++ src/main/java/com/restServer/demo/RZeroAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.restServer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RZeroAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RZeroAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restServer/demo/RZeroAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restServer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RZeroAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
