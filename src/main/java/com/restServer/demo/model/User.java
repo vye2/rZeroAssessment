2e99929e4b3e2916bd577333e01d7e74139b336c
vye2 <vye2@ucsc.edu>
2021-01-14 14:56:48 -0800

    config h2 database

--- src/main/java/com/restServer/demo/model/User.java
+++ src/main/java/com/restServer/demo/model/User.java
@@ -1,7 +1,12 @@
 package com.restServer.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id
 	private int id;
 	private String name;
 	
