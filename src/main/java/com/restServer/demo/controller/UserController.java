7dbaccedb405932b4e6c5014aa76f29b3c78aa3d
vye2 <vye2@ucsc.edu>
2021-01-14 15:54:01 -0800

    create Get and Post routes

--- src/main/java/com/restServer/demo/controller/UserController.java
+++ src/main/java/com/restServer/demo/controller/UserController.java
@@ -1,14 +1,38 @@
 package com.restServer.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restServer.demo.model.User;
 import com.restServer.demo.repository.UserRepository;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
 	UserRepository repository;
+	
+	
+	@PostMapping("users/add")
+	public User addUser(@RequestBody User user) {
+		repository.save(user);
+		return user;
+	}
+	
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return repository.findAll();
+	}
 
+	@GetMapping("/user/{id}")
+	public Optional<User> getUser(@PathVariable("id") int id) {
+		return repository.findById(id);
+	}
 }
--- src/main/java/com/restServer/demo/repository/UserRepository.java
+++ src/main/java/com/restServer/demo/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.restServer.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.restServer.demo.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 
 }
