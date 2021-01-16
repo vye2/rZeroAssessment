002d7b393ead8e2dd2dffb8d0852654c25235cd0
vye2 <vye2@ucsc.edu>
2021-01-15 20:38:51 -0800

    naming

---
+++ src/main/java/com/restServer/demo/config/PreloadDatabase.java
@@ -0,0 +1,36 @@
+package com.restServer.demo.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restServer.demo.model.User;
+import com.restServer.demo.service.UserService;
+
+@Configuration
+public class PreloadDatabase {
+	@Bean
+	CommandLineRunner preloadDB(UserService userService) {
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();		
+			TypeReference<List<User>> typeReference = new TypeReference<List<User>>(){};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/users.json");
+			try {
+				List<User> users = mapper.readValue(inputStream, typeReference);
+				for (User user: users) {
+					userService.addUser(user);
+				}
+				System.out.println("Saved " + users.size() + " initial users.");
+				
+			} catch (IOException e) {
+				System.out.println("Unable to save users: " + e.getMessage());
+			}
+		};
+	}
+}
