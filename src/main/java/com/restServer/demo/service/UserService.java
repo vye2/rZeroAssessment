65b1e67f6a17ef76cd68415a15b95a4ce9077f1e
vye2 <vye2@ucsc.edu>
2021-01-15 19:09:10 -0800

    put data manipulation logic in user service class

---
+++ src/main/java/com/restServer/demo/service/UserService.java
@@ -0,0 +1,36 @@
+package com.restServer.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.restServer.demo.model.User;
+import com.restServer.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public UserService(UserRepository userRepository) {
+		this.repository = userRepository;
+	}
+	
+	public User addUser(User user) {
+		repository.save(user);
+		return user;
+	}
+	
+	
+	public List<User> getUsers() {
+		return repository.findAll();
+	}
+	
+	public String removeUser(int id) {
+		repository.deleteById(id);
+		return "Deleted User " + id;
+		
+	}
+}
