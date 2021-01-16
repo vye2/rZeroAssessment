25f272ea915d079aaf9c2b5fc024be565480f1f9
vye2 <vye2@ucsc.edu>
2021-01-15 18:23:30 -0800

    finish basic get post and delete

--- src/main/java/com/restServer/demo/controller/UserController.java
+++ src/main/java/com/restServer/demo/controller/UserController.java
@@ -4,44 +4,51 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restServer.demo.model.User;
 import com.restServer.demo.repository.UserRepository;
 
 @RestController
+@RequestMapping(path="/users")
 public class UserController {
 	
+	
+	//Automatically inject UserRepository dependency.
 	@Autowired
 	UserRepository repository;
 	
 	
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getUsers() {
 		return repository.findAll();
 	}
 
-	@GetMapping("/user/{id}")
-	public Optional<User> getUser(@PathVariable("id") int id) {
-		return repository.findById(id);
-	}
+//	@GetMapping(path="/{id}")
+//	public Optional<User> getUser(@PathVariable("id") int id) {
+//		return repository.findById(id);
+//	}
 	
-	@PostMapping("users/add")
+	@PostMapping(path="/add", consumes= {"application/json"}, produces= {"application/json"})
 	public User addUser(@RequestBody User user) {
 		repository.save(user);
 		return user;
 	}
 	
-	@DeleteMapping("users/remove/{id}")
+	@DeleteMapping(path="/remove/{id}")
 	public String removeUser(@PathVariable("id") int id) {
 		User userToRemove = repository.getOne(id);
 		repository.delete(userToRemove);
-		return "User ID : " + id + " deleted.";
+		return "Deleted User with ID " +  id;
 	}
 	
 }
